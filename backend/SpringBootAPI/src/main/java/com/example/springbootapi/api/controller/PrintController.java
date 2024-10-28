4786f8cc7d62f939a572816c719f145570234499
lzieger03 <81645844+lzieger03@users.noreply.github.com>
2024-10-28 13:57:54 +0100

    PrintController

--- backend/SpringBootAPI/src/main/java/com/example/springbootapi/api/controller/PrintController.java
+++ backend/SpringBootAPI/src/main/java/com/example/springbootapi/api/controller/PrintController.java
@@ -7,15 +7,11 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.print.PrintService;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
